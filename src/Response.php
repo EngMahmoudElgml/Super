b48510d4138a602d0343ff4712c1b9925cba8fea
aya999 <pro.aya1993@gmail.com>
2023-05-15 15:57:57 +0300

    defalut response

--- src/Response.php
+++ src/Response.php
@@ -4,21 +4,21 @@ namespace EngMahmoudElgml\Super;
 
 class Response
 {
-    public function defaultResponse($status, $error_code, $validation, $message, $response, $token = NULL)
+    public static function defaultResponse($status, $error_code, $validation, $message, $response, $token = NULL)
     {
-        if (is_array($response)) {
-            if (count($response) == 0) {
-                $response = new \stdClass();
+            if (is_array($response)) {
+                if (count($response) == 0) {
+                    $response = new \stdClass();
+                }
             }
-        }
 
-        return response()->json([
-            'status' => $status,
-            'token' => $token,
-            'code' => $error_code,
-            'validation' => $validation,
-            'message' => $message,
-            'data' => $response,
-        ], $error_code);
-    }
+            return response()->json([
+                'status' => $status,
+                'token' => $token,
+                'code' => $error_code,
+                'validation' => $validation,
+                'message' => $message,
+                'data' => $response,
+            ], $error_code);
+        }
 }
